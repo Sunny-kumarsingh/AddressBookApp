34f925194f2b42414fb16dae80d7bf0ea89b445c
Sunny-kumarsingh <sunnnykumarsingh2711@gmail.com>
2026-03-09 01:35:10 +0530

    [sunny] added : UC22-read-entries-from-jsonserver

---
+++ src/main/java/com/addressbook/addressbookapp/model/AddressBookService.java
@@ -0,0 +1,16 @@
+package com.addressbook.addressbookapp.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBookService {
+	private List<Contact> addressBookMemory = new ArrayList<>();
+
+    public void updateMemory(List<Contact> contacts) {
+        addressBookMemory.addAll(contacts);
+    }
+
+    public List<Contact> getContacts() {
+        return addressBookMemory;
+    }
+}
